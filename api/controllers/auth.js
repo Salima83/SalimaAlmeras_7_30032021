0588ff1591cd7074b30ec56bd08bc90d532fb259
Salima <salima.almeras@laposte.net>
2023-04-30 20:02:36 +0100

    Ajout de la premiere version du projet

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,15 +1,30 @@
-export const register=(req,res)=>{
+import {db} from "../connect.js"
+import bcrypt from "bcryptjs";
+
+export const register = (req,res)=>{
   // check user if exists  
 
   const q ="SELECT FROM users WHERE username =?"
   db.query(q,[req.body.username],(err,data)=>{
 if(err) return res.status(500).json(err)
 if(data.lenght)return res.status(409).json("User already exists!")
-  })
+  
   //create a new user
   //Hash the password
-}
+  const salt =bcrypt.genSaltSync(10);
+  const hashedPassword=bcrypt.hasheSync(req.body.password, salt)
 
+  const q ="ISERT INTO  users (` username`,`email`,`password`,`name`) VALUE(?)"
+ 
+  const values=[req.body.username,req.body.email,hashedPassword,req.body.name]
+  
+  db.query(q,[values],(err,data)=>{
+    if(err)return res.status(500).json(err)
+    return res.status(200).json("User has been created.")
+
+  });
+});
+};
 
 export const login=(req,res)=>{
     
